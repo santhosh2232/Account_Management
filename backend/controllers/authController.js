729701f9d9026fd7a20e2a7fec50ff75d022e060
SanthoshKuwy <s.santhosh@kuwy.in>
2025-07-19 18:50:33 +0530

    Fix case in User model import

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
-const User = require('../models/User');
+const User = require('../models/user');
 const { Op } = require('sequelize');
 
 // @desc    Register user
